561be76d9f1c1124538123423d01f6e23483fa9f
nikhil-bacancy <nikhil.patel@bacancytechnology.com>
2019-01-08 17:17:44 +0530

    collect data and concate them from pipe

---
+++ httpCollect.js
@@ -0,0 +1,16 @@
+var bl = require('bl');
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url,function(response) {
+	 response.pipe(bl(function (err, data) {
+	 	if (err) {
+	 		return console.error(err);
+	 	}
+	 	console.log(data.length)
+	 	console.log(data.toString())
+
+	 }))  
+
+})
